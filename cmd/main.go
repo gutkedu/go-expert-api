68664b4114bba150bc6ba4898367193a78751e16
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2024-04-08 23:15:06 -0300

    feat: create product handler with test

--- cmd/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/gutkedu/go-expert-api/configs"
-
-func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBHost)
-}
---
+++ cmd/server/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gutkedu/go-expert-api/configs"
+	"github.com/gutkedu/go-expert-api/internal/dto"
+	"github.com/gutkedu/go-expert-api/internal/entity"
+	"github.com/gutkedu/go-expert-api/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	cfg, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	productDB := database.NewProduct(db)
+	productHandler := NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":"+cfg.WebServerPort, nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductRequest
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
